4eb106a112f4cd6ef9fa2ab078b62017c5b53f70
dagda25 <dagda25@gmail.com>
2020-05-16 13:55:47 +0300

    task 1-2

--- 01-javascript-data-types/2-pick/index.js
+++ 01-javascript-data-types/2-pick/index.js
@@ -5,5 +5,14 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
+    const picked = Object.entries(obj).filter((el) => {
+        for (let field of [...fields]) {
+            if (field === el[0]) {
+                return true
+            }
+        }
+        return false;
+    })
 
-};
+    return Object.fromEntries(picked);   
+};;
