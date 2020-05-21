b9458486efb0dae72559ccf074460192c7e886d6
dagda25 <dagda25@gmail.com>
2020-05-21 13:46:05 +0300

    task 2-5

--- 02-objects-arrays-intro-to-testing/5-sum/index.js
+++ 02-objects-arrays-intro-to-testing/5-sum/index.js
@@ -4,5 +4,20 @@
  * @returns {number | function}
  */
 export function sum (n) {
-
+    let result = n || 0;
+    
+    function repeat(x) {
+     if(x) {
+        result += x
+        return repeat
+     }
+     return result;
+    }
+    
+    repeat.toString = function() { 
+       return result;
+    }
+    
+    return repeat;
 }
+
