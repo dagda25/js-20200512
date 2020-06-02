92e4ee14a13b60fd677bfa09a2a3defbe944fe0a
dagda25 <dagda25@gmail.com>
2020-06-02 12:11:35 +0300

    task5 tooltip-attempt1

--- 05-events-practice/2-tooltip/src/index.js
+++ 05-events-practice/2-tooltip/src/index.js
@@ -1,5 +1,54 @@
 class Tooltip {
+    element; // HTMLElement;
+    elementWithTooltip;
 
+    onEnter = (event) => {
+        console.log(event.target);
+        if (event.target.dataset.tooltip != undefined) { 
+            this.elementWithTooltip = event.target;
+            event.target.addEventListener('pointermove', this.onMove)
+        }
+    }
+
+    onMove = (event) => {
+        this.elementWithTooltip.append(this.element);
+        this.elementWithTooltip.addEventListener('pointerout', this.onLeave)
+    }
+
+    onLeave = (event) => {
+        this.element.remove();
+        this.elementWithTooltip.removeEventListener('pointerout', this.onLeave);
+        this.elementWithTooltip.removeEventListener('pointermove', this.onMove)
+    }
+
+    constructor() {
+      this.render();
+    }
+
+    getTemplate() {
+        return `<div class="tooltip">This is tooltip</div>`;
+    }
+  
+    render() {
+        const element = document.createElement('div');
+
+        element.innerHTML = this.getTemplate();
+    
+        this.element = element.firstElementChild;
+    }
+
+    initialize() {
+        document.addEventListener('pointermove', this.onEnter); 
+    }
+  
+    remove () {
+      this.element.remove();
+    }
+  
+    destroy() {
+      this.remove();
+      document.removeEventListener('pointermove', this.onEnter); 
+    }
 }
 
 const tooltip = new Tooltip();
